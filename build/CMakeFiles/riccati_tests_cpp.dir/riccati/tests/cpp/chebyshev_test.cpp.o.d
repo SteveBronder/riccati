ce8b70c3853d009c936012bd8eb6ca583bf4055c
Steve Bronder <sbronder@flatironinstitute.org>
2023-11-14 18:20:08 -0500

    remove build folder

--- build/CMakeFiles/riccati_tests_cpp.dir/riccati/tests/cpp/chebyshev_test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/riccati_tests_cpp.dir/riccati/tests/cpp/chebyshev_test.cpp.o: \
- /mnt/home/sbronder/opensource/riccati/riccati/tests/cpp/chebyshev_test.cpp \
- /usr/include/stdc-predef.h
